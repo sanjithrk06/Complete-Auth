3e308d43f569ee39ed9ec1700b89a3e6f6e13366
sanjithrk06 <sanjithkarthikeyanr@gmail.com>
2024-10-14 14:29:42 +0530

    Auth Server setting up and signup controller completed

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,56 @@
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { generateTokenAndSetCookies } from "../utils/generateTokenAndSetCookie.js";
+
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required");
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const hashPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+    const user = new User({
+      email,
+      password: hashPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, //24 hours
+    });
+
+    await user.save();
+
+    //jwt
+    generateTokenAndSetCookies(res, user._id);
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  res.send("login route");
+};
+
+export const logout = async (req, res) => {
+  res.send("logout route");
+};
